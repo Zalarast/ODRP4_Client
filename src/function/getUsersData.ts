f9632cb513d419f39e3bf05b4ba3ce8c50841f70
Zalarast <shubin.bodya@gmail.com>
2025-05-20 20:23:31 +0300

    Доабавил фунцию для получения инфы об определённом человеке

--- src/function/getUsersData.ts
+++ src/function/getUsersData.ts
@@ -5,6 +5,14 @@ export async function getUsersData(searchNickName: string) {
     return axios({
         url: serverUrl() + "/getInfo",
         method: "POST",
-        data: { searchNickName: searchNickName }
+        data: { searchNickName, type: "findeAll" }
+    })
+}
+
+export async function getUserInfo(searchSteamId: string) {
+    return axios({
+        url: serverUrl() + "/getInfo",
+        method: "POST",
+        data: { searchSteamId, type: "findeOne" }
     })
 }
